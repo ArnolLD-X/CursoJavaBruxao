f3bb987a9b2072b409bdfcba3636f87708f20123
Artur Pendragon <152702507+KingArTrx@users.noreply.github.com>
2023-12-22 11:54:58 -0300

    cxc

---
+++ aula11/ex08.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { //Condição simples
+    console.log(`Mais exemploss : bobby!`)
+}
+console.log(`Diriga sempre sem cinto de segurança`)
---
+++ aula11/ex09.js
@@ -0,0 +1,7 @@
+var país = 'África do Sul'
+console.log(`Vivendo em ${país}`)
+if (país == 'África do Sul') {
+    console.log('Você é Africano! (//_-)')
+} else {
+    console.log('Você é Turista ^^')
+}
